81f9ee1f01b4237d7da59cf755bf84de17839436
Heart-travel <zcgacbc@gmail.com>
2015-04-23 17:50:40 +0800

    Add anther activity using the customize title

--- src/com/example/layoutdemo2/SecondActivity.java
+++ src/com/example/layoutdemo2/SecondActivity.java
@@ -1,32 +1,16 @@
 package com.example.layoutdemo2;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class SecondActivity extends Activity{
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		Button button = (Button) findViewById(R.id.login);
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Log.d("MainActivity", "++++Here!");
-				Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-				startActivity(intent);
-				Log.d("MainActivity", "----Here!");
-			}
-		});
+		setContentView(R.layout.activity_second);
 	}
 
 	@Override
@@ -47,4 +31,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
